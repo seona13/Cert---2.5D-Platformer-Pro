9c5db87e4c92758657298045f81fe705006106d5
Seona Bellamy <seona13@gmail.com>
2021-09-03 08:43:08 +1000

    Player Movement - Gravity
    * Added a gravity variable to the Player script.
    
    * If Player is not grounded, apply gravity to the velocity to bring them back to a platform (or into the void if they missed!).

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Player : MonoBehaviour
 {
     private CharacterController _controller;
     [SerializeField]
     private float _speed = 5f;
+    [SerializeField]
+    private float _gravity = 1f;
 
 
 
@@ -18,13 +21,19 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetAxis("Horizontal") != 0)
-		{
-            float horizontalInput = Input.GetAxis("Horizontal");
-            Vector3 direction = new Vector3(horizontalInput, 0, 0);
-            Vector3 velocity = direction * _speed;
+        float horizontalInput = Input.GetAxis("Horizontal");
+        Vector3 direction = new Vector3(horizontalInput, 0, 0);
+        Vector3 velocity = direction * _speed;
 
-            _controller.Move(velocity * Time.deltaTime);
-		}
+        if (_controller.isGrounded)
+	    {
+            // jump?
+	    }
+        else
+	    {
+            velocity.y -= _gravity;
+	    }
+
+        _controller.Move(velocity * Time.deltaTime);
     }
 }
